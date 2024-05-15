9135eb4ae02f7f9a0946d5230e1a7fa677d6ed40
niranjankumar-c <niranjankumar.c@outlook.com>
2024-05-15 19:28:57 +0530

    updated openai and pinecone parameters

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv() #load the .env variables
 
-#set the config parameters
+#set the config parameters for minio
 MINIO_ENDPOINT = "play.min.io:9000"
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
@@ -11,6 +11,11 @@ MINIO_BUCKET_NAME = "my-bucket2"
 
 # Define the output directory for saving files
 OUTPUT_DIR = os.path.join(os.getcwd(), "ocr_results")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+#pinecone parameters
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENV = os.getenv("PINECONE_ENV")
+
+#openai parameters
+EMBEDDING_MODEL_ID = os.getenv("OPENAI_EMBEDDING_MODEL")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
